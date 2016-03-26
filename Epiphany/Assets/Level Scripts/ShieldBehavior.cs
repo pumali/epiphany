548c2e5e2dbff1a7698074ab61cd9e7d53226fa8
chakabaka1027 <alexanderdcha@gmail.com>
2016-03-25 21:14:06 -0400

    added level 2 and put in shield for first puzzle of level1

--- Epiphany/Assets/Level Scripts/ShieldBehavior.cs
+++ Epiphany/Assets/Level Scripts/ShieldBehavior.cs
@@ -7,10 +7,16 @@ public class ShieldBehavior : MonoBehaviour {
 	public GameObject Pickup2;
 	public GameObject Pickup3;
 	public PlayerController Player;
+	public BridgeStates BridgeStatus;
 
 	// Use this for initialization
 	void Start () {
-	
+		GameObject PlayerBall = GameObject.FindGameObjectWithTag("Player");
+		Player = PlayerBall.GetComponent<PlayerController> ();
+
+		GameObject BridgeCubes = GameObject.FindGameObjectWithTag("Bridge1");
+		BridgeStatus = BridgeCubes.GetComponent<BridgeStates> ();
+
 	}
 	
 	// Update is called once per frame
@@ -19,14 +25,12 @@ public class ShieldBehavior : MonoBehaviour {
 	}
 
 	IEnumerator OnTriggerEnter(Collider trigger){
-		if (trigger.gameObject.CompareTag("Player")){
+		if (trigger.gameObject.CompareTag("Player") && BridgeStatus.BridgeState1 == false){
 
-			GameObject PlayerBall = GameObject.FindGameObjectWithTag("Player");
-			Player = PlayerBall.GetComponent<PlayerController> ();
+			
 			Player.isCollecting = "Beginning";
 
 			gameObject.GetComponent<BoxCollider>().enabled = false;
-//			yield return new WaitForSeconds(0.5f);
 			Pickup1.SetActive(false);
 			Pickup2.SetActive(false);
 			Pickup3.SetActive(false);
@@ -38,7 +42,7 @@ public class ShieldBehavior : MonoBehaviour {
 			Pickup3.SetActive(true);
 			gameObject.GetComponent<BoxCollider>().enabled = true;
 			yield return new WaitForSeconds(2);
-
+	
 
 		}
 	}
