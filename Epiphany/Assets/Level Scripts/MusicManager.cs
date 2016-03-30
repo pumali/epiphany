efe8d403812fa5fd543e067578c5df0538e5a021
chakabaka1027 <alexanderdcha@gmail.com>
2016-03-29 21:51:13 -0400

    eliminated transitions

--- Epiphany/Assets/Level Scripts/MusicManager.cs
+++ Epiphany/Assets/Level Scripts/MusicManager.cs
@@ -26,21 +26,21 @@ public class MusicManager : MonoBehaviour {
 
 
 		if (PuzzleStatus == "Beginning") {
-			tutorialBegin.TransitionTo (0.3f);
+			tutorialBegin.TransitionTo (0f);
         }
 
 		if (PuzzleStatus == "Puzzle1") {
-			tutorialPuzzle1.TransitionTo (0.3f);
+			tutorialPuzzle1.TransitionTo (0f);
 
         }
 
 		if (PuzzleStatus == "Puzzle2") {
-			tutorialPuzzle2.TransitionTo (0.3f);
+			tutorialPuzzle2.TransitionTo (0f);
 
         }
 
 		if (PuzzleStatus == "Puzzle3") {
-			tutorialPuzzle3.TransitionTo (0.3f);
+			tutorialPuzzle3.TransitionTo (0f);
 
         }
 	}
