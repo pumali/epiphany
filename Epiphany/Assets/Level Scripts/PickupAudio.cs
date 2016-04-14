9cc3f3f81dc371c79fd3d6682847d3fefbd91d1f
lo <soc@soc-mac-image-4.american.edu>
2016-04-13 22:38:37 -0400

    different audio code combos

--- Epiphany/Assets/Level Scripts/PickupAudio.cs
+++ Epiphany/Assets/Level Scripts/PickupAudio.cs
@@ -4,14 +4,23 @@ using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 public class PickupAudio : MonoBehaviour {
 
-	public AudioSource pickup; 
+	public AudioClip pickupSound; 
+	public AudioClip fastforward; 
 
- IEnumerator OnTriggerEnter(Collider other)
+	private AudioSource sound; 
+
+	void Awake ()
+	{ 
+		sound = GetComponent<AudioSource> (); 
+	}
+
+	void Update ()
 	{
-		if (other.gameObject.tag == "Player") {
-
-			//Debug.Log ("about to play pickup " + other.gameObject.tag);
-			GetComponent<AudioSource>().Play(); 
-		}
+	 OnTriggerEnter (Collider other) 
+		{
+			if (other.gameObject.tag == "Player") {
+		sound.Play ()  
+	}
+}
 	}
 }
