41606de635541d4d9052bf3fe47841f4bf947e90
chakabaka1027 <alexanderdcha@gmail.com>
2016-04-18 12:17:22 -0400

    added scene management to goal script

--- Epiphany/Assets/Level Scripts/Goal.cs
+++ Epiphany/Assets/Level Scripts/Goal.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour {
 
 public GameObject Light;
+public float fadeTime = 0.8f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,12 +17,16 @@ public GameObject Light;
 
 	}
 
-	void OnTriggerEnter(Collider trigger) 
+	IEnumerator OnTriggerEnter(Collider trigger) 
 	{
-		if (trigger.gameObject.CompareTag("Player"))
+		if (trigger.gameObject.CompareTag("Player") && gameObject.CompareTag("Goal"))
 			{
 			gameObject.SetActive (false);
 			Light.SetActive (false);
+
+			fadeTime = GameObject.Find("Player").GetComponent<Fading>().BeginFade(1);
+			yield return new WaitForSeconds(fadeTime);
+			SceneManager.LoadScene(0 + 1);
 			}
 	
 	}
