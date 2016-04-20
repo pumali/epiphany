29e68cee5bd97099fd0477df9cfd322e63aa6b3b
pumali <patricia.l.umali@gmail.com>
2016-04-19 22:56:38 -0400

    changed snapshots to array
    in MusicManager script

--- Epiphany/Assets/Level Scripts/MusicManager.cs
+++ Epiphany/Assets/Level Scripts/MusicManager.cs
@@ -5,9 +5,9 @@ using UnityEngine.Audio;
 public class MusicManager : MonoBehaviour {
 
 	public string PuzzleStatus = "Beginning";
-	public AudioSource audio;
+	public AudioSource music;
 
-	public AudioMixerSnapshot tutorialBegin; 
+	public AudioMixerSnapshot[] songSnapshots = new AudioMixerSnapshot[4]; 
 	public AudioMixerSnapshot tutorialPuzzle1;
 	public AudioMixerSnapshot tutorialPuzzle2;
 	public AudioMixerSnapshot tutorialPuzzle3;
@@ -15,32 +15,29 @@ public class MusicManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	audio = GetComponent<AudioSource>();
+	music = GetComponent<AudioSource>();
 	}
 
 	// Update is called once per frame
 	void Update () {
 
 
-
-
-
 		if (PuzzleStatus == "Beginning") {
-			tutorialBegin.TransitionTo (0f);
+			songSnapshots[0].TransitionTo (0f);
         }
 
-		if (PuzzleStatus == "Puzzle1") {
-			tutorialPuzzle1.TransitionTo (0f);
-
-        }
-
-		if (PuzzleStatus == "Puzzle2") {
-			tutorialPuzzle2.TransitionTo (0f);
+		else if (PuzzleStatus == "Puzzle1") {
+			songSnapshots[1].TransitionTo (0f);
 
         }
 
-		if (PuzzleStatus == "Puzzle3") {
-			tutorialPuzzle3.TransitionTo (0f);
+		else if (PuzzleStatus == "Puzzle2") {
+            songSnapshots[2].TransitionTo (0f);
+
+        }
+
+		else if (PuzzleStatus == "Puzzle3") {
+            songSnapshots[3].TransitionTo (0f);
 
         }
 	}
