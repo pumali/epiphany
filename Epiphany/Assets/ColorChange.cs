5b7311e8c181cf347550a38cd4e8dc2ef4848bd2
chakabaka1027 <alexanderdcha@gmail.com>
2016-04-21 21:10:44 -0400

    level painting functioning

---
+++ Epiphany/Assets/ColorChange.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorChange : MonoBehaviour {
+
+	public Color RedColorChange;
+
+	void OnCollisionEnter(Collision trigger) 
+	{
+		if (trigger.gameObject.CompareTag("Player")){
+		gameObject.GetComponent<Renderer> ().material.color = RedColorChange;
+		}
+	}
+}
