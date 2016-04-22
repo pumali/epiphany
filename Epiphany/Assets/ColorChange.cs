cb2f289c7d8b7b7ec502591622e7b6defea7a4a1
chakabaka1027 <alexanderdcha@gmail.com>
2016-04-21 21:21:06 -0400

    color returns with time

--- Epiphany/Assets/ColorChange.cs
+++ Epiphany/Assets/ColorChange.cs
@@ -4,11 +4,17 @@ using System.Collections;
 public class ColorChange : MonoBehaviour {
 
 	public Color RedColorChange;
+	public float ColorChangeTimer = 4;
+	public Color RedColorReturn;
 
-	void OnCollisionEnter(Collision trigger) 
+
+	IEnumerator OnCollisionEnter(Collision trigger) 
 	{
 		if (trigger.gameObject.CompareTag("Player")){
 		gameObject.GetComponent<Renderer> ().material.color = RedColorChange;
+		yield return new WaitForSeconds(ColorChangeTimer);
+		gameObject.GetComponent<Renderer> ().material.color = RedColorReturn;
+
 		}
 	}
 }
