bbb16a22615c4ef596b912003fe3c9c8ba7ab950
chakabaka1027 <alexanderdcha@gmail.com>
2016-04-23 21:24:35 -0400

    level design tweaks

---
+++ Epiphany/Assets/Level Scripts/PuzzleColorChanger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleColorChanger : MonoBehaviour {
+	public PlayerController Player;
+	public Color RedColorChange;
+	public GameObject[] Puzzle1;
+
+
+	// Use this for initialization
+	void Start () {
+		GameObject PlayerBall = GameObject.FindGameObjectWithTag("Player");
+		Player = PlayerBall.GetComponent<PlayerController> ();
+
+		Puzzle1 = GameObject.FindGameObjectsWithTag ("Puzzle1Neutrals");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+	IEnumerator ColorChange(){
+		if (Player.isCollecting == "Third") {
+			foreach(GameObject form in Puzzle1) {
+				yield return new WaitForSeconds(0.1f);
+				form.gameObject.GetComponent<Renderer> ().material.color = RedColorChange;
+			}
+		}
+	}
+}
