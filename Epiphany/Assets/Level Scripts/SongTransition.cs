b34fe61935d1f81a8272f794e7cb07f16883864f
SOC <soc@soc-mac-image-4.american.edu>
2016-04-26 19:16:30 -0400

    finalized audio transition

---
+++ Epiphany/Assets/Level Scripts/SongTransition.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SongTransition : MonoBehaviour {
+
+	public int destroyedmusiclevel;
+	public string LevelName;
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	void OnLevelWasLoaded(int level) {
+		if (level == destroyedmusiclevel)
+			Destroy (gameObject);
+	}
+}
