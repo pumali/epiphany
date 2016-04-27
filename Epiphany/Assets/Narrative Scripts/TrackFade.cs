4c301ddfbee573f1d978a90e1de7785a17dab831
SOC <soc@soc-mac-image-4.american.edu>
2016-04-26 20:44:43 -0400

    added narrative audio

--- Epiphany/Assets/Narrative Scripts/TrackFade.cs
+++ Epiphany/Assets/Narrative Scripts/TrackFade.cs
@@ -5,13 +5,24 @@ using UnityEngine.SceneManagement;
 
 
 public class TrackFade : MonoBehaviour {
+	public AudioSource audioSource;
 
 	public int NextScene;
 	public float fadeTime = 0.8f;
+	public AudioClip Narrative;
+	public float NarrativeTimer;
+	public AudioClip Tape;
+
 
 	// Use this for initialization
 	IEnumerator Start () {
+	audioSource = GetComponent<AudioSource>();
 
+	audioSource.PlayOneShot(Narrative, 0.7f);
+
+	yield return new WaitForSeconds(NarrativeTimer);
+
+	audioSource.PlayOneShot(Tape, 0.7f);
 
 
 	yield return new WaitForSeconds(3);
